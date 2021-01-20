fa161bae97cfc7c55bd6259fcc26f6acf4ade38d
ilya <54044644+ipisman-costar@users.noreply.github.com>
2021-01-19 17:57:43 -0800

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ description       'Installs and configures postgresql for clients or servers'
 version           '8.1.1'
 source_url        'https://github.com/sous-chefs/postgresql'
 issues_url        'https://github.com/sous-chefs/postgresql/issues'
-chef_version      '>= 13.8'
+chef_version      '>= 12.2'
 
 depends 'yum-epel'
 depends 'apt'
